33498519d1cf0484e9f001677dc8ede2358678c8
OrangeHarry <hhm12345@naver.com>
2022-01-03 19:15:30 +0900

    1/3 Coding_test Syudy

---
+++ src/com/harry/test/CodingTest6.java
@@ -0,0 +1,22 @@
+package com.harry.test;
+//두 정수 사이의 합
+public class CodingTest6 {
+	    public long solution(int a, int b) {
+	        long answer = 0; 
+	        
+	        if(a<b){
+	            for(int i=a; i<=b; i++){
+	                answer += i;
+	            }
+	        }else if(a>b){
+	            for(int i=b; i<=a; i++){
+	                answer += i;
+	            }
+	        }else{
+	            answer = a;
+	        }
+	        
+	        
+	        return answer;
+	    }
+}
